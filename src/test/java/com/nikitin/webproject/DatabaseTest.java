d3cd991aaadc3168001e17af84f74482b2e4b39e
Rus Nik <nikitin_r_b@meta.ua>
2018-02-26 19:29:49 +0200

    JUnit test added.

---
+++ src/test/java/com/nikitin/webproject/DatabaseTest.java
@@ -0,0 +1,34 @@
+package com.nikitin.webproject;
+
+import org.junit.After;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ *  For testing the connection.
+ *  Connects to the actual db (fleet_of_vehicles_db).
+ */
+public class DatabaseTest {
+    private String URL = "jdbc:mysql://localhost:3306/fleet_of_vehicles_db";
+    private String USERNAME = "root";
+    private String PASSWORD = "root";
+    private String DRIVER = "com.mysql.jdbc.Driver";
+    private Connection conn;
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void connectTest() throws SQLException, ClassNotFoundException {
+        Class.forName(DRIVER);
+        conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        assertNotNull(conn);
+    }
+}
